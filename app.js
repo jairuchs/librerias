41b014dd62af765cbc9b3cb36530edb61c496c94
Jairo Cruz <jairolucianocruz@gmail.com>
2022-09-19 15:18:52 -0300

    trabajo final

--- app.js
+++ app.js
@@ -157,18 +157,21 @@ const accionBotones = () => {
     })
 }
 
-const url = 'https://pokeapi.co/api/v2/pokemon/14/'
 
-fetch(url)
-.then(response => response.json())
-.then(data => {
-    let element = document.getElementById("elem")
-    element.innerHTML=`
-    <p>${data.name}</p>
-    <p>${data.order}</p>
-    <img src='${data.sprites.front_default}'/>
-    `; 
-    console.log(data)
-})
+function finalizarCompra(){
 
-.catch(err=>console.log(err))
+    Swal.fire(
+        'compra finalizada!',
+        'muchas gracias!',
+        'success'
+      )
+}
+
+function consultaRealizada(){
+    Swal.fire(
+        'consulta realizada!',
+        'muchas gracias!',
+        'success'
+      )
+
+}
